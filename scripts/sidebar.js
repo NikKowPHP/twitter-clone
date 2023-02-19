58c4a1213396b80b37715e663ab0753f7774733a
NikKowPHP <nik.kow@outlook.com>
2023-02-19 09:51:53 +0100

    modify with additional active class and pointer events

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -5,10 +5,17 @@ const sidebar_profile_tooltip = document.querySelector("#sidebar_profile_tooltip
 sidebar_profile.addEventListener("click", function(event) {
     if(sidebar_profile.contains(event.target)) {
         sidebar_profile_tooltip.style.opacity = 1;
+        sidebar_profile_tooltip.style.pointerEvents= "all";
+        sidebar_profile.classList.add("sidebar-active");
     }
 });
+
+if(sidebar_profile_tooltip.style.opacity === 1) {
+}
 document.onclick = function(event) {
     if(event.target !== sidebar_profile && event.target !== sidebar_profile_tooltip && !sidebar_profile.contains(event.target)) {
         sidebar_profile_tooltip.style.opacity= 0;
+        sidebar_profile_tooltip.style.pointerEvents= "none";
+        sidebar_profile.classList.remove("sidebar-active");
     }
 }
