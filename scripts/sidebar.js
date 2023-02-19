b24125d5a2803a37c814ae81be328f489ab60b35
NikKowPHP <nik.kow@outlook.com>
2023-02-19 09:35:51 +0100

    create onclick events for profile tooltips

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,1 +1,14 @@
-let = 
+const sidebar_profile = document.querySelector("#sidebar_profile");
+const sidebar_profile_tooltip = document.querySelector("#sidebar_profile_tooltip");
+
+
+sidebar_profile.addEventListener("click", function(event) {
+    if(sidebar_profile.contains(event.target)) {
+        sidebar_profile_tooltip.style.opacity = 1;
+    }
+});
+document.onclick = function(event) {
+    if(event.target !== sidebar_profile && event.target !== sidebar_profile_tooltip && !sidebar_profile.contains(event.target)) {
+        sidebar_profile_tooltip.style.opacity= 0;
+    }
+}
