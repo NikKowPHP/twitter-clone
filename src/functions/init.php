7ff9d09dc2e48505bae9a4a71a83b6314a9c5260
NikKowPHP <nik.kow@outlook.com>
2023-02-24 18:56:24 +0100

    modify constants with links to folders

--- src/functions/init.php
+++ src/functions/init.php
@@ -1,7 +1,9 @@
 <?php 
-define("ROOT", "/twitter-clone/");
+define("ROOT", "/twitter-clone");
 define("DS", DIRECTORY_SEPARATOR);
 define("CLASSES", ROOT . DS . 'src' . DS . 'classes');
-require_once(CLASSES . DS . 'Db_object.php');
-require_once(CLASSES . DS . 'User.php');
+
+require_once("../" . DS .CLASSES.DS. "Database.php");
+require_once("../". DS .CLASSES . DS . 'Db_object.php');
+require_once("../" . DS .CLASSES . DS . 'User.php');
 ?>
