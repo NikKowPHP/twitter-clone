c0258912b4a03f4801c63cb7685f1a26fc48058d
NikKowPHP <nik.kow@outlook.com>
2023-02-24 18:57:36 +0100

    create field and main methods for Database class

--- src/classes/Database.php
+++ src/classes/Database.php
@@ -1,5 +1,26 @@
-<?php 
+<?php
+
+use function PHPSTORM_META\type;
+
+require_once('./config/db_config.php');
 class Database {
+    public $conn;
 
 
-} ?>
+    public function open_connection() {
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        if($this->conn->connect_errno) {
+            die("Connection failed: " . $this->conn->connect_errno);
+        }
+    }
+    public function query(string $sql) {
+        $query = $this->conn->query($sql);
+        echo gettype($query);
+    }
+
+
+
+} 
+$database = new Database();
+
+?>
