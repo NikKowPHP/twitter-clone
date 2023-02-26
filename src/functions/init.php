25c3a84a536f88a12aba44e793f8c4fbd0caace8
NikKowPHP <nik.kow@outlook.com>
2023-02-26 08:39:32 +0100

    add session class to init

--- src/functions/init.php
+++ src/functions/init.php
@@ -3,6 +3,7 @@ define("ROOT", "/twitter-clone");
 define("DS", DIRECTORY_SEPARATOR);
 define("CLASSES", ROOT . DS . 'src' . DS . 'classes');
 
+require_once("../" . DS .CLASSES . DS . 'Session.php');
 require_once("../" . DS .CLASSES.DS. "Database.php");
 require_once("../". DS .CLASSES . DS . 'Db_object.php');
 require_once("../" . DS .CLASSES . DS . 'User.php');
