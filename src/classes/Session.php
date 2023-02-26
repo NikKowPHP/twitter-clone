aa476258728b6361fd23bac2b13d45bece998409
NikKowPHP <nik.kow@outlook.com>
2023-02-26 08:30:41 +0100

    add methods: construct, set and get message

--- src/classes/Session.php
+++ src/classes/Session.php
@@ -2,9 +2,36 @@
 session_start();
 
 class Session {
-    public string $message;
+    private ?string $message;
 
-    
+    function __construct() {
+        if(isset($_SESSION['message'])) {
+            $this->message = $_SESSION['message'];
+        } else {
+            $this->message = null;
+        }
+    }
+
+    public function set_message($message):void
+    {
+        $this->message = $message;
+        $_SESSION['message'] = $message;
+    }
+
+    public function get_message(): ?string
+    {
+        if(!is_null($this->message)) {
+           $output = "<div class=\"message\">"; 
+           $output .= htmlentities($this->message);
+           $output .= "</div>";
+           $this->message = null;
+           return $output;
+        } else {
+            return null;
+        }
+
+    }
 }
+$session = new Session();
 
 ?>
