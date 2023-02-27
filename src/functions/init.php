e6972e86e0acdf71ca6a63c999b0a48216918fe4
NikKowPHP <nik.kow@outlook.com>
2023-02-27 08:14:05 +0100

    redefine constants, modify roots

--- src/functions/init.php
+++ src/functions/init.php
@@ -1,10 +1,11 @@
 <?php 
-define("ROOT", "/twitter-clone");
+define("ROOT", "C:/MAMP/htdocs/twitter-clone");
 define("DS", DIRECTORY_SEPARATOR);
 define("CLASSES", ROOT . DS . 'src' . DS . 'classes');
 
-require_once("../" . DS .CLASSES . DS . 'Session.php');
-require_once("../" . DS .CLASSES.DS. "Database.php");
-require_once("../". DS .CLASSES . DS . 'Db_object.php');
-require_once("../" . DS .CLASSES . DS . 'User.php');
+
+require_once(CLASSES . DS . "Session.php");
+require_once(CLASSES . DS . "Database.php");
+require_once(CLASSES . DS . 'Db_object.php');
+require_once(CLASSES . DS . 'User.php');
 ?>
