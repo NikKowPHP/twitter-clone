1cd874296fa08b7477f768af6f70c61d7bed96c9
NikKowPHP <nik.kow@outlook.com>
2023-02-27 08:15:11 +0100

    create password encryption and password check methods

--- src/classes/User.php
+++ src/classes/User.php
@@ -8,5 +8,29 @@ class User extends Db_object{
     public ?string $password;
     public ?string $email;
 
+   public function encrypt_password() {
+        $blowfish_hash_format = "$2y$10$";
+        $salt_length = 22;
+        $salt = $this->generate_salt($salt_length);
+        $formatted_salt = $blowfish_hash_format . $salt;
+        $this->password = crypt($this->password, $formatted_salt);
+    }
+    protected function generate_salt($length) 
+    {
+        $random_str = md5(uniqid(mt_rand(), true));
+        $base64_str = base64_encode($random_str);
+        $modified_base64_str = str_replace('+', '.', $base64_str);
+        $salt = substr($modified_base64_str,0, $length);
+        return $salt;
+    }
+    public function password_check($password, $existing_hash) {
+        $hash = crypt($password, $existing_hash);
+        if($hash === $existing_hash) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
 ?>
