d6fa39b5e5a20a2a55d784851be6f68a0661779e
NikKowPHP <nik.kow@outlook.com>
2023-03-01 18:00:08 +0100

    separate modals to different files

---
+++ partials/modals/modal_login.php
@@ -0,0 +1,20 @@
+
+    <div id="modal-login" class="modal modal-register modal-login">
+      <div class="modal-content modal-content-register">
+        <span class="close">&times;</span>
+        <div class="modal-body">
+
+
+
+                
+          <h3>Log in into your account</h3>
+
+          <form action="src/includes/login_verification.php" method="POST">
+
+                <input type="email" name="email"  placeholder="E-mail">
+                <input type="password" name="password"  placeholder="Password">
+                <input type="submit" value="Log in" name="login_submit" class="btn btn-register-submit">
+          </form>
+        </div>
+      </div>
+    </div>
---
+++ partials/modals/modal_register.php
@@ -0,0 +1,32 @@
+
+    <div id="modal-register" class="modal modal-register">
+      <div class="modal-content modal-content-register">
+        <span class="close">&times;</span>
+        <div class="modal-body">
+
+          <pre>
+
+          <?php 
+          
+          ?>
+          </pre>
+
+          <h3>Create your account</h3>
+
+
+            <?php echo $session->get_message() ?>
+
+          <form action="src/includes/register.php.php" method="POST">
+
+                <input type="text" name="username"  placeholder="Name">
+                <input type="email" name="email"  placeholder="E-mail">
+                <input type="password" name="password"  placeholder="Password">
+                <input type="submit" value="Sign Up" name="register_submit" class="btn btn-register-submit">
+
+          </form>
+                
+
+
+        </div>
+      </div>
+    </div>
