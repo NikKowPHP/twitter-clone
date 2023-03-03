be33a1ce635d27ccf58b52cffa025ea375b95a5b
NikKowPHP <nik.kow@outlook.com>
2023-03-03 18:06:26 +0100

    change open_connection function to fit with pdo

--- src/classes/Database.php
+++ src/classes/Database.php
@@ -4,7 +4,7 @@ use function PHPSTORM_META\type;
 
 require_once('/var/www/html/twitter-clone/config/db_config.php');
 class Database {
-    public $conn;
+    public $pdo;
 
 
     function __construct() {
@@ -12,10 +12,27 @@ class Database {
 
     }
     public function open_connection() {
-        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        if($this->conn->connect_errno) {
-            die("Connection failed: " . $this->conn->connect_errno);
+
+        try {
+            $this->pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES);
+            $this->pdo->beginTransaction();
+        } catch (PDOException $e) {
+            die("Connection falied: " . $e->getMessage());
         }
+
+    }
+    public function execute_query(string $sql, array $params = array() ){
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            foreach($params as $key => $value) {
+                
+            }
+
+        }
+
+
     }
     public function query(string $sql){
         if($result = $this->conn->query($sql)) {
