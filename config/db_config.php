e8b9b20046d9eb3598335ae33074ab2c74b96729
NikKowPHP <nik.kow@outlook.com>
2023-03-03 18:06:04 +0100

    change configuration for database file

--- config/db_config.php
+++ config/db_config.php
@@ -4,4 +4,5 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'mikita');
 define('DB_PASSWORD', 'gans2vlad');
 define('DB_NAME', 'twitter');
+define('DB_DSN', "mysql:host=" . DB_HOST. ";"."dbname=".DB_NAME);
 ?>
