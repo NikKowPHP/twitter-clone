92a819068c1ac2350277751bdcaea525d37e0c56
NikKowPHP <nik.kow@outlook.com>
2023-03-05 08:09:29 +0100

    add tweet class to init

--- src/functions/init.php
+++ src/functions/init.php
@@ -8,5 +8,6 @@ require_once(CLASSES . DS . "Session.php");
 require_once(CLASSES . DS . "Database.php");
 require_once(CLASSES . DS . 'Db_object.php');
 require_once(CLASSES . DS . 'User.php');
+require_once(CLASSES . DS . 'Tweet.php');
 require_once(ROOT .DS."src".DS."functions".DS. "functions.php");
 ?>
