f59f525a5829ea293bcac61dc3a295ead5093374
NikKowPHP <nik.kow@outlook.com>
2023-03-05 08:10:33 +0100

    modify db_fields prop

--- src/classes/User.php
+++ src/classes/User.php
@@ -3,7 +3,7 @@ require_once("Session.php");
 
 class User extends Db_object{
     protected static string $db_table_name = "users";
-    protected static array $db_fields = ['username', 'password', 'email', 'token', 'active'];
+    protected static array $db_fields; 
     public ?int $id;
     public ?string $username;
     public ?string $password;
