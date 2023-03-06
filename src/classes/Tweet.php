c873c2f830ec3d605f8f6eb3388b10b5d70707db
NikKowPHP <nik.kow@outlook.com>
2023-03-06 20:12:11 +0100

    add props to tweet class

--- src/classes/Tweet.php
+++ src/classes/Tweet.php
@@ -1,7 +1,15 @@
 <?php 
 class Tweet extends Db_object 
 {
+    protected string $db_table_name = "tweets";
+    protected array $db_fields;
+
+    public ?int $id;
+    public ?int $user_id;
+    public ?string $body;
+    public ?string $image;
+    public ?string $date;
+    
 
 }
  ?>
- <!-- TODO: continue work on tweet class -->
