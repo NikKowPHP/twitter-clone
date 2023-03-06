632c53a2c55262ab64be77997d43423e0582607f
NikKowPHP <nik.kow@outlook.com>
2023-03-06 20:11:31 +0100

    modify verify login

--- src/includes/login_verification.php
+++ src/includes/login_verification.php
@@ -4,9 +4,9 @@
 require_once("../functions/init.php");
 
 if(isset($_POST['login_submit'])) {
-    $input_user = User::iterate_through_post($_POST);
-    print_r($input_user);
-    if($user_db = $input_user->check()) {
+    $input_user = new User();
+    $input_user->iterate_through_post($_POST);
+    if($db_user = $input_user->check()) {
         redirect("index.php");
     }
 }
