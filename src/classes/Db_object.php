50a8fdd2bb66982f6e9b3cc7a08018b1ce73d4d7
NikKowPHP <nik.kow@outlook.com>
2023-03-06 20:09:45 +0100

    change: props from static to non, additional methods to get props, constructor, modify methods: instance, get_data_by_query

--- src/classes/Db_object.php
+++ src/classes/Db_object.php
@@ -1,17 +1,20 @@
 <?php
 class Db_object
 {
-    protected static string $db_table_name = '';
-    protected  static array $db_fields = array();
+    protected string $db_table_name = '';
+    protected array $db_fields = array();
 
     public function __construct()
     {
-        self::$db_fields = $this->get_db_fields();
+       $this->db_fields =  $this->get_db_fields();
+       $this->db_table_name = $this->get_table_name();
+        
     }
     protected function get_db_fields(): array
     {
-        $sql = "describe ". static::$db_table_name; 
+        $sql = "describe ". $this->db_table_name; 
         global $database;
+        
         $db_all_columns = $database->execute_query(sql:$sql, return_data: true);
         
         $db_columns = [];
@@ -21,30 +24,50 @@ class Db_object
             $db_columns[] = $db_column['Field'];
           }
         }
+        
        return $db_columns; 
 
     }
-    private static function instantiate($db_row)
+    protected function get_table_name():string
     {
-        $calling_class = get_called_class();
-        $obj = new $calling_class;
+        return strtolower(get_called_class()) . 's';
+    }
+    public function get_table_name_string():string
+    {
+        return $this->db_table_name;
+    }
+    public function get_db_fields_array(): array
+    {
+        return $this->db_fields;
+    }
+    protected function instantiate($db_row):void
+    {
+        
         foreach ($db_row as $key => $value) {
-            if (property_exists($obj, $key)) {
-                $obj->$key = $value;
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
             }
         }
-        return $obj;
 
     }
     public static function get_data_by_query(string $sql, array $params = array(), $return_data = true)
     {
         global $database;
+
+
         $objects_arr = [];
         $rows = $database->execute_query($sql, $params, $return_data);
             foreach($rows as $row) {
-                $objects_arr[] = static::instantiate($row);
+
+                $new_obj = static::create_object(); 
+                $new_obj->instantiate($row);
+                $objects_arr[] = $new_obj;
             }
         return $objects_arr;
+    }
+    protected static function create_object():static
+    {
+        return new static();
     }
     public static function get_all() {
         $sql = "SELECT * FROM " . static::$db_table_name;
@@ -58,17 +81,16 @@ class Db_object
         $sql = "SELECT * FROM $table $where LIMIT 1";
         return static::get_data_by_query($sql, $params, true);
     }
-    public static function iterate_through_post(array $post):self 
+    public  function iterate_through_post(array $post):void 
     {
-        global $database;
-        $obj_props = static::$db_fields;
+        $obj_props = $this->db_fields;
         $obj_array = [];
         foreach($post as $key => $value) {
             if(in_array($key,$obj_props)) {
-               $obj_array[$key] = trim($value); 
+               $this->$key = trim($value); 
             }
         }
-        return static::instantiate($obj_array);
+        
     }
     protected function properties(): array
     {
