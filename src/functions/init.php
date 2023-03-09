080974d0024614468b4324215bf4bf7be3a2d39f
NikKowPHP <nik.kow@outlook.com>
2023-03-09 07:28:29 +0100

    init file handler

--- src/functions/init.php
+++ src/functions/init.php
@@ -8,6 +8,7 @@ require_once(CLASSES . DS . "Session.php");
 require_once(CLASSES . DS . "Database.php");
 require_once(CLASSES . DS . 'Db_object.php');
 require_once(CLASSES . DS . 'User.php');
+require_once(CLASSES . DS . 'File_handler.php');
 require_once(CLASSES . DS . 'Tweet.php');
 require_once(ROOT .DS."src".DS."functions".DS. "functions.php");
 ?>
