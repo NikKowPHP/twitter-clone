0bfbc3f2a9c2e15bd0df14c9bdb6f5ff04b9b196
NikKowPHP <nik.kow@outlook.com>
2023-03-16 17:33:30 +0100

    change following_user functionallity: from POST to GET. create unfollow functionallity

--- src/includes/following_user.php
+++ src/includes/following_user.php
@@ -1,13 +1,19 @@
 <?php
 require_once("/var/www/html/twitter-clone/src/functions/init.php");
 
-if(isset($_POST['follow'])) {
+if(isset($_GET['follow'])) {
     $user_id = $session->get_user_id();
     $follow = new Follow();
     $follow->set_user_id($session->get_user_id());
-    $follow->set_following($_GET['following']);
+    $follow->set_following($_GET['follow']);
     if($follow->create()) {
-        redirect("index.php");
+        reload();
     }
+}
+if (isset($_GET['unfollow'])) {
+    $user_id = $session->get_user_id();
+    $follow = Follow::get_by_id($_GET['unfollow']);
+    $follow->delete();
+    reload();
 }
 ?>
