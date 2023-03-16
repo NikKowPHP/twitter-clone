0f2c1410cbd06ad5bed4b06bba9ada99f3c78b91
NikKowPHP <nik.kow@outlook.com>
2023-03-16 17:32:12 +0100

    change method redirect: add exit func create reload func

--- src/functions/functions.php
+++ src/functions/functions.php
@@ -1,6 +1,12 @@
 <?php 
 function redirect(string $location) {
     header("Location:/twitter-clone/$location");
+    exit();
+}
+function reload()
+{
+    header("Location: {$_SERVER['HTTP_REFERER']}");
+    exit();
 }
 
 
