4a0e3743f3e6a06c7969312212d5fe02fc516f32
NikKowPHP <nik.kow@outlook.com>
2023-03-19 08:01:13 +0100

    add cookies to init

--- src/functions/init.php
+++ src/functions/init.php
@@ -12,5 +12,6 @@ require_once(CLASSES . DS . 'File_handler.php');
 require_once(CLASSES . DS . 'Tweet.php');
 require_once(CLASSES . DS . 'Follow.php');
 require_once(CLASSES . DS . 'Modal.php');
+require_once(CLASSES . DS . 'Cookie.php');
 require_once(ROOT .DS."src".DS."functions".DS. "functions.php");
 ?>
