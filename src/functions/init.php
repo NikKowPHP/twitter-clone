732545d863e737c1207bedbe8620197c15c410f8
NikKowPHP <nik.kow@outlook.com>
2023-03-22 15:52:56 +0100

    create a new class Authentication and add to init list

--- src/functions/init.php
+++ src/functions/init.php
@@ -13,5 +13,6 @@ require_once(CLASSES . DS . 'Tweet.php');
 require_once(CLASSES . DS . 'Follow.php');
 require_once(CLASSES . DS . 'Modal.php');
 require_once(CLASSES . DS . 'Cookie.php');
+require_once(CLASSES . DS . 'Authentication.php');
 require_once(ROOT .DS."src".DS."functions".DS. "functions.php");
 ?>
