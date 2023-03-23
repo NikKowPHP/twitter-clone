68728581ad4ce5a03887f96304ce40524288d27a
NikKowPHP <nik.kow@outlook.com>
2023-03-23 09:33:43 +0100

    change the logic of login verification

--- src/includes/login_verification.php
+++ src/includes/login_verification.php
@@ -1,15 +1,12 @@
-<pre>
-
-<?php 
+<?php
 require_once("../functions/init.php");
 
-if(isset($_POST['login_submit'])) {
-    $input_user = User::iterate_through_post($_POST);
-    if($db_user = $input_user->check()) {
-        redirect("index.php");
-    }
-}
-
-
+if(isset($_POST['login_submit']))
+   if(Authentication::login($_POST['email'], $_POST['password'])) {
+    Session::set('is_logged_in', 'email or password are wrong');
+       redirect('index.php');
+   }else {
+       Session::set('is_logged_in', 'email or password are wrong');
+       redirect('login_page.php');
+   }
 ?>
-</pre>
