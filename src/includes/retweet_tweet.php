32581b874023b50578ba469116e048c223d52315
NikKowPHP <nik.kow@outlook.com>
2023-03-27 19:36:13 +0200

    create untweet functionallity

--- src/includes/retweet_tweet.php
+++ src/includes/retweet_tweet.php
@@ -4,7 +4,14 @@ if(isset($_GET['tweet'])) {
     $retweet = new Retweet();
     $retweet->setTweetId((int)$_GET['tweet']);
     $retweet->setUserId(Cookie::get('user_id'));
+
     $retweet->create();
+//    TODO: if exist untweet
     redirect('index.php');
+}
+if(isset($_GET['untweet'])) {
+    $retweet = Retweet::get_by_id($_GET['untweet']);
+    $retweet->delete();
+    reload();
 }
 ?>
