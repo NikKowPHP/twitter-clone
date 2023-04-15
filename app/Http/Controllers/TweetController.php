689e5d5546dd295dc31bdc37e43ea4fd5728d2af
NikKowPHP <nik.kow@outlook.com>
2023-04-15 09:02:30 +0200

    show index page

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class TweetController extends Controller
 {
+    // Show welcome page
     public function welcome() {
         return view('users.initial');
+    }
+    // Show index page
+    public function index() {
+        return view('users.index');
     }
 }
