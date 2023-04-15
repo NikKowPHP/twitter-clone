594bafdcfa10283f021f1b9507062554db63b7e8
NikKowPHP <nik.kow@outlook.com>
2023-04-15 09:02:14 +0200

    create a store function to write to database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+
+    // Create a new user
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:6'],
+
+        ]);
+
+        //Hash 
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        //Create user
+        $user = User::create($formFields);
+
+        //login 
+        auth()->login($user);
+        return redirect('/home')->with('message', 'Welcome to your personalized home page');
+
+    }
 }
