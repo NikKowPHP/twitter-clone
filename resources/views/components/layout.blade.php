b6dd57b331cfd289450e6d98e10d9e2783c30f47
NikKowPHP <nik.kow@outlook.com>
2023-04-15 10:18:07 +0200

    put partials to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,4 +1,3 @@
-@include('modals.modal-login')
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -214,53 +213,8 @@
 
         </div>
       </nav>
-    </header>
 
-
-        <nav class="flex justify-between items-center mb-4">
-            <a href="/"
-                ><img class="w-24" src="{{asset('images/logo.png')}} " alt="" class="logo"
-            /></a>
-            <ul class="flex space-x-6 mr-6 text-lg">
-                @auth
-                    
-                <li>
-                    <span class="font-bold uppercase">
-                        Welcome {{auth()->user()->name}}
-                    </span>
-                </li>
-                <li>
-                    <a href="/listings/manage" class="hover:text-laravel"
-                        ><i class="fa-solid fa-gear"></i>
-                        Manage Listings</a
-                    >
-                </li>
-                <li>
-                    <form class="inline" action="/logout" method="post">
-                    @csrf
-                    <button type="submit" class="fa-solid fa-door-closed">Logout</button>
-                
-                </form>
-                </li>
-                @else
-
-                <li>
-                    <a href="/register" class="hover:text-laravel"
-                        ><i class="fa-solid fa-user-plus"></i> Register</a
-                    >
-                </li>
-                <li>
-                    <a href="/login" class="hover:text-laravel"
-                        ><i class="fa-solid fa-arrow-right-to-bracket"></i>
-                        Login</a
-                    >
-                </li>
-                @endauth
-            </ul>
-        </nav>
-
-
-
+      {{-- NAV END --}}
 
 
 
@@ -269,19 +223,132 @@
         </main>
 
 
-        <footer
-            class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
-        >
-            <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
-
-            <a
-                href="/listings/create"
-                class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
-                >Post Job</a
+<aside>
+    <div class="search-bar-container">
+        <div class="search-bar-icon">
+            <svg
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                    class="r-14j79pv r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-4wgw6l r-f727ji r-bnwqim r-1plcrui r-lrvibr"
             >
-        </footer>
+                <g>
+                    <path
+                            d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"
+                    ></path>
+                </g>
+            </svg>
+        </div>
+        <input class="search-bar" type="text" placeholder="Search Twitter"/>
+    </div>
 
-        <x-flash-message />
+    <div class="aside-section hashtags-container">
+        <div class="aside-section-header hashtags-header">
+            <h6>Trands for you</h6>
+        </div>
+
+        <div class="hashtag-container">
+            <div class="hashtag-header">
+                <span class="dimmed-text">Trending</span>
+                <svg
+                        viewBox="0 0 24 24"
+                        aria-hidden="true"
+                        class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
+                >
+                    <g>
+                        <path
+                                d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"
+                        ></path>
+                    </g>
+                </svg>
+            </div>
+            <div class="hashtag-name">TypeScript</div>
+            <div class="hashtag-number dimmed-text">42.3K Tweets</div>
+        </div>
+
+        <div class="hashtag-container">
+            <div class="hashtag-header">
+                <span class="dimmed-text">Sports &#183; Trending</span>
+                <svg
+                        viewBox="0 0 24 24"
+                        aria-hidden="true"
+                        class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
+                >
+                    <g>
+                        <path
+                                d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"
+                        ></path>
+                    </g>
+                </svg>
+            </div>
+            <div class="hashtag-name">#BarcaMUFC</div>
+            <div class="hashtag-number dimmed-text">42.3K Tweets</div>
+        </div>
+        <div class="show-more-btn hashtag">Show more</div>
+    </div>
+
+    <div class="aside-section">
+        <div class="aside-section-header">
+            <h6>Who to follow</h6>
+        </div>
+
+        <?php $channels = User::get_all(); ?>
+        <?php foreach ($channels as $channel): ?>
+            <?php if ($channel->id != $user_id = Cookie::get('user_id')): ?>
+
+
+                <div class="channel-container">
+                    <div class="channel-image">
+                        <img src="images/<?= $channel->image ?>" alt="<?= $channel->username; ?> logo"/>
+                    </div>
+                    <div class="channel-info">
+                        <div class="channel-name"><?= $channel->username ?></div>
+                        <div class="dimmed-text">@<?= $channel->username ?></div>
+                    </div>
+                    <div class="follow-btn-container">
+
+                        <?php if ($user->is_following($channel->id)): ?>
+                            <?php
+                            $arr = ['user_id' => $user_id, 'following' => $channel->id];
+                            $follow = Follow::get_by($arr);
+                            ?>
+
+                            <a href="src/includes/following_user.php?unfollow=<?= $follow->id ?>"
+                               class="btn follow-btn">Unfollow</a>
+
+                        <?php else: ?>
+
+                            <a href="src/includes/following_user.php?follow=<?= $channel->id ?>" class="btn follow-btn">Follow</a>
+
+                        <?php endif; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+        <?php endforeach; ?>
+
+
+        <div class="show-more-btn hashtag">Show more</div>
+    </div>
+
+    <div class="aside-section terms-data">
+        <a href="">Terms of Service</a>
+        <a href="">Privacy Policy</a>
+        <a href="">Cookie Policy</a>
+        <br>
+        <a href="">Accessibility</a>
+        <a href="">Ads info</a>
+        <a href="">More</a>
+        <br>
+        <span>&#169; 2023 Twitter-copy, Inc.</span>
+    </div>
+</aside>
+
+    </div> 
+
+    <script src="{{ URL::asset('js/header.js') }} "></script>
+    <script src="{{ URL::asset('js/sidebar.js') }} "></script>
+    <script src="{{ URL::asset('js/create_tweet.js') }} "></script>
+    <script src="{{ URL::asset('js/icons_handler.js') }} "></script>
+    <script src="{{ URL::asset('js/modal.js') }} "></script>
 
 </body>
 
