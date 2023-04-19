d8e2e5bad109ba8e18a890916f88519414162294
NikKowPHP <nik.kow@outlook.com>
2023-04-19 10:18:11 +0200

    define relationships between follow and user

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Follow extends Model
 {
     use HasFactory;
+
+
+    public function followedUser()
+    {
+        return $this->belongsTo(User::class, 'followed_user_id');
+    }
+    public function followingUser()
+    {
+        return $this->belongsTo(User::class, 'following_user_id');
+    }
 }
