2757b149a4862b440d3874b75af102c139450ef9
NikKowPHP <nik.kow@outlook.com>
2023-04-23 08:04:12 +0200

    add filter to exclude active user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -57,5 +58,10 @@ class User extends Authenticatable
     public function following()
     {
         return $this->hasMany(Follow::class, 'following_user_id');
+    }
+
+    public function scopeExcludeAuth($query) {
+        return $query->whereNotIn('id', [Auth::user()->id]);
+
     }
 }
