70b9febe98c2dac1838282ec191b055bfe5661a5
NikKowPHP <nik.kow@outlook.com>
2023-05-09 16:34:28 +0200

    add signup page path

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,6 +1,11 @@
 import {createBrowserRouter} from "react-router-dom";
+import Signup from "./views/Signup";
 
 const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Signup />
+	}
 
 ]);
 
