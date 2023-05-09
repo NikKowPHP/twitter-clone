87f029a6e41428c23d0ead473eb0d663e05b04d0
NikKowPHP <nik.kow@outlook.com>
2023-05-09 16:48:19 +0200

    create context for user and token, define token

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -1,2 +1,29 @@
 import { createContext, useState } from "react";
 
+const StateContext = createContext({
+	currentUser: null,
+	token: null
+});
+
+export const ContextProvider = ({child}) => {
+	const [user, setUser] = useState({});
+	const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+	const setToken = (token) => {
+		_setToken(token);
+		if(token) {
+			localStorage.setItem('ACCESS_TOKEN', token);
+		} else {
+			localStorage.removeItem('ACCESS_TOKEN');
+		}
+
+	}
+
+	return (
+		<StateContext.Provider value={{
+
+		}}>
+
+		</StateContext.Provider>
+	)
+
+}
