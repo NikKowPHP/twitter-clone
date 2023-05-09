d0d3af18b198f344071786c7b9937a0f93b64392
NikKowPHP <nik.kow@outlook.com>
2023-05-09 16:29:30 +0200

    create signup page

---
+++ react/src/views/Signup.jsx
@@ -0,0 +1,54 @@
+
+// @extends('components.layout')
+
+
+
+// @section('main')
+import React from 'react'
+
+export default function Signup() {
+	return (
+		<div>Signup</div>
+	)
+}
+
+
+//         <div class="header-center-container">
+//             <div class="home-link-container">
+//                 <a class="home-link" href="/home">Home</a>
+//             </div>
+//             <div id="header-buttons" class="header-buttons-container">
+//                 <a href="#" class="header-button-link active-home-link"><span>For you</span> </a>
+//                 <a href="#" class="header-button-link"><span> Following</span></a>
+//             </div>
+//         </div>
+
+//               @include('partials._create_tweet')
+
+//         <div class="show-new-tweets-container">
+//             <p>Show 425 Tweets</p>
+//         </div>
+        
+
+
+//         <section class="posts">
+
+
+// 	    @unless (count($tweets) == 0)
+//             @foreach($tweets as $tweet)
+
+//             <x-card :tweet="$tweet" :retweets="$retweets" :user="$user"/>
+
+//             @endforeach
+//         @else
+//             <p>No tweets found</p>
+//         @endunless						
+
+
+
+
+//         </section>
+
+// @endsection
+
+
