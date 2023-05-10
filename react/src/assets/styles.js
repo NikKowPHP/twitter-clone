1fd6e6e6ec378471719c7bf8d1f6eb486a3a630d
NikKowPHP <nik.kow@outlook.com>
2023-05-10 19:44:20 +0200

    import css with one js

---
+++ react/src/assets/styles.js
@@ -0,0 +1,9 @@
+import '../styles/general.css';
+import '../styles/header.css';
+import '../styles/sidebar.css';
+import '../styles/main.css';
+import '../styles/aside.css';
+import '../styles/footer.css';
+import '../styles/modal.css';
+
+export default null;
