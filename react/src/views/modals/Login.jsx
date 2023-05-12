e79466db67bc7121f93d196f2f7dbcc73789a49d
NikKowPHP <nik.kow@outlook.com>
2023-05-12 08:09:20 +0200

    create  a modal login

--- react/src/views/modals/Login.jsx
+++ react/src/views/modals/Login.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ReactDOM } from 'react'
+import Modal from './Modal'
 
 export default function Login() {
 	return (
-
-		<div>
-    	<div id="modal-login" className="modal modal-register modal-login">
-      <div className="modal-content modal-content-register">
-        <button type="button" className="close btn-empty" data-dismiss="modal" aria-label="Close">
-          <span class="close" aria-hidden="true">&times;</span>
-        </button>
-        <div className="modal-body">
+		<>
+      <Modal >
           <h3>Log in into your account</h3>
 
           <form action="/users/authenticate" method="POST">
@@ -17,10 +13,9 @@ export default function Login() {
                 <input type="password" name="password"  placeholder="Password"/>
                 <input type="submit" value="Log in" name="login_submit" className="btn btn-register-submit" />
           </form>
-        </div>
-      </div>
-    </div>
-		</div>
+
+      </Modal>
+		</>
 
 
 
